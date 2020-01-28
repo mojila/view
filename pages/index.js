dbd12fdc5d7c2fd7e91becaa6b073c29921ba6d1
Mojila <me.mojila@gmail.com>
2020-01-29 02:06:33 +0700

    init

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import fetch from 'isomorphic-unfetch'
+
+function HomePage({ stars }) {
+  return <React.Fragment>
+      <head>
+          <title>Coba</title>
+      </head>
+      
+      <div>Next stars: {stars} -> get from server</div>
+  </React.Fragment>
+}
+
+HomePage.getInitialProps = async () => {
+  const res = await fetch('https://api.github.com/repos/zeit/next.js')
+  const json = await res.json()
+  return { stars: json.stargazers_count }
+}
+
+export default HomePage
